1f86009c9c989211f9ec2ad2a97f21b2cfd59882
Vikky <41500162+vikky-noelle@users.noreply.github.com>
2020-04-24 09:19:30 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -9,7 +9,7 @@ from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 def index(request):
-    response = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?lat=23.20&lon=77.08&APPID=3ea8c4603341150369ad240ffece6189')
+    response = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?lat=23.20&lon=77.08&APPID=YOUR_ID')
     jsonResponse = json.load(response)
     temp = jsonResponse.get('main',{}).get('temp',{})-273.15
     pres = jsonResponse.get('main',{}).get('pressure',{})
